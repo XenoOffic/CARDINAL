b00ec6796bfb2794c3098951a3b76ab6fbf77ab8
XenoOffic <uservinted7106@gmail.com>
2026-09-22 15:19:34 +0200

    Add diagnostics to module exports

--- language/__init__.py
+++ language/__init__.py
@@ -4,5 +4,14 @@ CARDINAL language package.
 Contains the grammar, lexer, parser, AST,
 type system and semantic analysis components.
 """
+from .diagnostics import (
+    Diagnostic,
+    DiagnosticSeverity,
+    SourceLocation,
+)
 
-__all__ = []
+__all__ = [
+    "Diagnostic",
+    "DiagnosticSeverity",
+    "SourceLocation",
+]
