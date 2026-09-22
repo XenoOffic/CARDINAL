d0508fd1e729288055ae39d998452843a1bb2ad4
XenoOffic <uservinted7106@gmail.com>
2026-09-22 21:46:21 +0200

    Add operand_stack to frame class

--- runtime/vm/frame.py
+++ runtime/vm/frame.py
@@ -10,4 +10,5 @@ class CallFrame:
     function_name: str
     instruction_pointer: int = 0
     locals: dict[str, object] = field(default_factory=dict)
+    operand_stack: list[object] = field(default_factory=list)
     return_value: object | None = None
