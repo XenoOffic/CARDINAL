9484b0e70bf6042967e756f152123e7424df376e
XenoOffic <uservinted7106@gmail.com>
2026-09-22 16:18:05 +0200

    Add __init__.py for language.types module

---
+++ language/types/__init__.py
@@ -0,0 +1,23 @@
+from .type import (
+    ANY,
+    BOOL,
+    FLOAT,
+    INT,
+    STRING,
+    UNIT,
+    UNKNOWN,
+    CardinalType,
+    TypeKind,
+)
+
+__all__ = [
+    "ANY",
+    "BOOL",
+    "FLOAT",
+    "INT",
+    "STRING",
+    "UNIT",
+    "UNKNOWN",
+    "CardinalType",
+    "TypeKind",
+]
