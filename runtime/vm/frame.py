1bca2c95042ec0c74965287e42228959442d0da0
XenoOffic <uservinted7106@gmail.com>
2026-09-23 16:16:45 +0200

    Add scope management methods to frame class

--- runtime/vm/frame.py
+++ runtime/vm/frame.py
@@ -12,3 +12,42 @@ class CallFrame:
     locals: dict[str, object] = field(default_factory=dict)
     operand_stack: list[object] = field(default_factory=list)
     return_value: object | None = None
+
+    scopes: list[dict[str, object]] = field(
+        default_factory=list
+    )
+
+    def __post_init__(self) -> None:
+        if not self.scopes:
+            self.scopes.append(self.locals)
+        else:
+            self.locals = self.scopes[0]
+
+    def enter_scope(self) -> None:
+        self.scopes.append({})
+
+    def exit_scope(self) -> None:
+        if len(self.scopes) <= 1:
+            raise RuntimeError(
+                "Cannot exit function root scope."
+            )
+
+        self.scopes.pop()
+
+    def declare(self, name: str, value: object) -> None:
+        self.scopes[-1][name] = value
+
+    def lookup(self, name: str) -> object:
+        for scope in reversed(self.scopes):
+            if name in scope:
+                return scope[name]
+
+        raise KeyError(name)
+
+    def assign(self, name: str, value: object) -> None:
+        for scope in reversed(self.scopes):
+            if name in scope:
+                scope[name] = value
+                return
+
+        raise KeyError(name)
