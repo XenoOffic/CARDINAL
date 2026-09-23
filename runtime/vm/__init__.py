a700f919ed40f0410f81ab847af2bf9a752846d4
XenoOffic <uservinted7106@gmail.com>
2026-09-23 18:00:51 +0200

    Refactor imports and update __all__ in init.py

--- runtime/vm/__init__.py
+++ runtime/vm/__init__.py
@@ -1,9 +1,31 @@
 from .frame import CallFrame
-from .vm import AgentInstance, VM, VMError
+from .messaging import (
+    AgentMessage,
+    MessageBus,
+)
+from .scheduler import (
+    Scheduler,
+    SchedulerStats,
+)
+from .vm import (
+    AgentContext,
+    AgentEvent,
+    AgentInstance,
+    AgentLifecycle,
+    VM,
+    VMError,
+)
 
 __all__ = [
     "CallFrame",
+    "AgentContext",
+    "AgentEvent",
     "AgentInstance",
+    "AgentLifecycle",
+    "AgentMessage",
+    "MessageBus",
+    "Scheduler",
+    "SchedulerStats",
     "VM",
     "VMError",
 ]
